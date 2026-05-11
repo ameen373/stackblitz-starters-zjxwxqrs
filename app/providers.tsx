6764be4134e2d829ede840e04e001e80bd8095f6
ameen373 <81308096+ameen373@users.noreply.github.com>
2026-05-11 22:58:09 +0300

    Create Providers component for React Query and TelegramAuth
    Implement a Providers component to wrap children with QueryClientProvider and TelegramAuth.

---
+++ app/providers.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { useState } from "react";
+
+import TelegramAuth from "@/components/telegram/TelegramAuth";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            retry: 1,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TelegramAuth />
+
+      {children}
+    </QueryClientProvider>
+  );
+}
