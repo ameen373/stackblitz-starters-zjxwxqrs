48ca1471c345bba5d73fd7053daf925258cae6ee
ameen373 <81308096+ameen373@users.noreply.github.com>
2026-05-11 22:07:59 +0300

    Implement authentication store using Zustand

---
+++ stores/auth.store.ts
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  telegramId: string;
+  username?: string;
+  firstName?: string;
+  lastName?: string;
+  photoUrl?: string;
+}
+
+interface AuthState {
+  user: User | null;
+
+  token: string | null;
+
+  isAuthenticated: boolean;
+
+  setAuth: (token: string, user: User) => void;
+
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+
+  token: null,
+
+  isAuthenticated: false,
+
+  setAuth: (token, user) => {
+    localStorage.setItem("token", token);
+
+    set({
+      token,
+      user,
+      isAuthenticated: true,
+    });
+  },
+
+  logout: () => {
+    localStorage.removeItem("token");
+
+    set({
+      token: null,
+      user: null,
+      isAuthenticated: false,
+    });
+  },
+}));
